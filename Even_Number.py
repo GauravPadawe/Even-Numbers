b3a0d4a0a2725e7f85a26a8e3e8dde7a3ca0bc87
Gaurav Padawe <grvpdw92@gmail.com>
2018-06-13 23:42:41 +0530

    Initial commit

---
+++ Even_Number.py
@@ -0,0 +1,12 @@
+﻿a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+def div(a):                # Define a procedure
+    x = []                 # Empty list
+    for i in a:            # for i in list a
+        if i % 2 == 0:     # i % 2 == 0 then appen append empty list
+            x.append(i)
+    print (x)              # print x
+            
+print (div(a))
+
+# CODED BY - GAURAV PADAWE
